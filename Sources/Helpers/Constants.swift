3e346b5a7e5c370cf0c45f45900b1a77b57880cb
Unknown <chanhee.kim00@gmail.com>
2018-05-02 15:20:32 +0900

    Configure Project Structure

---
+++ Sources/Helpers/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  ToonMaker
+//
+//  Created by CHANHEE KIM on 2018. 5. 2..
+//  Copyright © 2018년 CHK. All rights reserved.
+//
+
+import Foundation
+
+enum Config {
+    static let baseURL = NSURL(string: "http://www.example.org/")!
+    static let splineReticulatorName = "foobar"
+}
+
+//enum Color {
+//    static let primaryColor = UIColor(red: 0.22, green: 0.58, blue: 0.29, alpha: 1.0)
+//    static let secondaryColor = UIColor.lightGray
+//    
+//    // A visual way to define colours within code files is to use #colorLiteral
+//    // This syntax will present you with colour picker component right on the code line
+//    static let tertiaryColor = #colorLiteral(red: 0.22, green: 0.58, blue: 0.29, alpha: 1.0)
+//}
